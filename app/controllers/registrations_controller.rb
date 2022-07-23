1874d6d8e5f2a8fa4dd5d6dd1cb9f98ce8cac817
Andrea Vecino <andreavecino94@gmail.com>
2022-07-22 20:30:33 -0500

    add image params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,6 +14,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
   def sign_up_params
-    params.require(:user).permit(:email, :password, :name, :last_name)
+    params.require(:user).permit(:email, :password, :name, :last_name, :image)
   end
 end
