f1c654f45a4f05f4233c31e33fbebe8635499c19
andreavecino <andreavecino94@gmail.com>
2025-01-21 17:48:17 -0500

    ruby update

--- config/initializers/logger_patch.rb
+++
@@ -1,8 +0,0 @@
-module ActiveSupport
-  module LoggerThreadSafeLevel
-    Logger::Severity.constants.each do |severity| # Cambia esto si la constante no existe
-      const_set severity, Logger::Severity.const_get(severity)
-    end
-  end
-end
-
